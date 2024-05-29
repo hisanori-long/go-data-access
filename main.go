47c1fd8f7abb75d3976ec6f3febc1318448fbf8f
Hisanori Shimakawa <shimakawa.hisanori@gmail.com>
2024-05-30 01:41:27 +0900

    access JSON data

--- main.go
+++ main.go
@@ -1,18 +1,42 @@
 package main
 
 import (
-	"github.com/PuerkitoBio/goquery"
+	"encoding/json"
+	"io/ioutil"
+	"net/http"
 )
 
 func main() {
-	p := "https://golang.org"
-	doc, er := goquery.NewDocument(p)
+	p := "https://tuyano-dummy-data.firebaseio.com/mydata.json"
+	re, er := http.Get(p)
+	if er != nil {
+		panic(er)
+	}
+	defer re.Body.Close()
+
+	s, er := ioutil.ReadAll(re.Body)
 	if er != nil {
 		panic(er)
 	}
 
-	doc.Find("a").Each(func(n int, sel *goquery.Selection) {
-		lk, _ := sel.Attr("href")
-		println(n, sel.Text(), "(", lk, ")")
-	})
+	var itms []Mydata
+	er = json.Unmarshal(s, &itms)
+	if er != nil {
+		panic(er)
+	}
+
+	for i, im := range itms {
+		println(i, im.Str())
+	}
+}
+
+type Mydata struct {
+	Name string
+	Mail string
+	Tel  string
+}
+
+// Str get string value.
+func (m *Mydata) Str() string {
+	return "<\"" + m.Name + "\" " + m.Mail + " " + m.Tel + ">"
 }
