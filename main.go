76be4c2cb33908927be470b7860fb6dd6a2cac75
Hisanori Shimakawa <shimakawa.hisanori@gmail.com>
2024-05-30 01:01:34 +0900

    read text file

--- main.go
+++ main.go
@@ -2,36 +2,30 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
 	"os"
 )
 
 func main() {
-	// write text function
-	wt := func(f *os.File, s string) {
-		_, er := f.WriteString(s + "\n")
+	// read text function
+	rt := func(f *os.File) {
+		s, er := ioutil.ReadAll(f)
 		if er != nil {
 			panic(er)
 		}
+		fmt.Println(string(s))
 	}
 
 	fn := "data.txt"
 
-	f, er := os.OpenFile(fn, os.O_APPEND|os.O_CREATE|os.O_WRONLY, os.ModePerm)
+	f, er := os.OpenFile(fn, os.O_RDONLY, os.ModePerm)
 	if er != nil {
 		panic(er)
 	}
-	fmt.Println("**** start ****")
-	wt(f, "**** start ****")
 
-	s := "hello go world!"
-	wt(f, s)
-
-	wt(f, "**** end ****\n\n")
-	fmt.Println("**** end ****")
-	er = f.Close()
-	if er != nil {
-		panic(er)
-	}
+	fmt.Println("<<< start >>>")
+	rt(f)
+	fmt.Println("<<< end >>>")
 
 	defer f.Close()
 }
