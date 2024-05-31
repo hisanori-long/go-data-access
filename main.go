0b0eb1735ab97f32b666703e61bbbbc7c7eead2e
Hisanori Shimakawa <shimakawa.hisanori@gmail.com>
2024-06-01 00:01:54 +0900

    use Exec

--- main.go
+++ main.go
@@ -21,7 +21,7 @@ func (m *Mydata) Str() string {
 	return "<\"" + strconv.Itoa(m.ID) + ":" + m.Name + "\" " + m.Mail + "," + strconv.Itoa(m.Age) + ">"
 }
 
-var qry string = "select * from mydata where name like ? or mail like ?"
+// var qry string = "select * from mydata where name like ? or mail like ?"
 
 func main() {
 	con, er := sql.Open("sqlite3", "data.sqlite3")
@@ -30,21 +30,41 @@ func main() {
 	}
 	defer con.Close()
 
-	s := "happy" // input
-	if s == "" {
-		panic("no input")
+	nm := "TaroTaro" // input
+	ml := "taro@yeah"
+	age := "32"
+	ag, _ := strconv.Atoi(age)
+
+	qry := "insert into mydata (name, mail, age) values (?, ?, ?)"
+	con.Exec(qry, nm, ml, ag)
+	showRecord(con)
+}
+
+// print all records.
+func showRecord(con *sql.DB) {
+	qry := "select * from mydata"
+	rs, _ := con.Query(qry)
+	for rs.Next() {
+		fmt.Println(mydatafmRws(rs).Str())
 	}
-	rs, er := con.Query(qry, "%"+s+"%", "%"+s+"%")
+}
+
+// get mydata from Rows.
+func mydatafmRws(rs *sql.Rows) *Mydata {
+	var md Mydata
+	er := rs.Scan(&md.ID, &md.Name, &md.Mail, &md.Age)
 	if er != nil {
 		panic(er)
 	}
+	return &md
+}
 
-	for rs.Next() {
-		var md Mydata
-		er := rs.Scan(&md.ID, &md.Name, &md.Mail, &md.Age)
-		if er != nil {
-			panic(er)
-		}
-		fmt.Println(md.Str())
+// get Mydata from Row.
+func mydatafmRw(rs *sql.Row) *Mydata {
+	var md Mydata
+	er := rs.Scan(&md.ID, &md.Name, &md.Mail, &md.Age)
+	if er != nil {
+		panic(er)
 	}
+	return &md
 }
